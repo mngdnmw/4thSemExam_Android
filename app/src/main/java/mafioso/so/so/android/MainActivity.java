3667e14b5aa13d5c65216ab7aca60d1f237c3802
Jeppe Ehmsen <jeppeehmsen@gmail.com>
2018-04-23 14:02:31 +0200

    Ready for test

--- app/src/main/java/mafioso/so/so/android/MainActivity.java
+++ app/src/main/java/mafioso/so/so/android/MainActivity.java
@@ -1,9 +1,17 @@
 package mafioso.so.so.android;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +21,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> test = new HashMap<>();
+        test.put("text", "test");
+
+        db.collection("test")
+                .add(test)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("FIRE", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("FIRE", "Error adding document", e);
+                    }
+                });
+
     }
 }
