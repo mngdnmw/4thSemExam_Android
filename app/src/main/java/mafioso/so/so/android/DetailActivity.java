f182760ac31c3ab13656b7d2012784b88c8cbb43
Jeppe Ehmsen <jeppeehmsen@gmail.com>
2018-05-15 12:06:57 +0200

    Update

--- app/src/main/java/mafioso/so/so/android/DetailActivity.java
+++ app/src/main/java/mafioso/so/so/android/DetailActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import mafioso.so.so.android.BE.PictureBE;
 import mafioso.so.so.android.DAL.DAO;
@@ -13,11 +14,18 @@ public class DetailActivity extends AppCompatActivity {
     /** --- Tag for debug logging. --- */
     String TAG = "SOSOMAFIOSO::MAIN";
 
-    PictureBE picture;
+    PictureBE m_picture;
 
     DAO m_DAO;
 
     ImageView imageView_detailPicture;
+
+    TextView textView_name;
+    TextView textView_location;
+    TextView textView_disResult;
+    TextView textView_cityResult;
+    TextView textView_weaResult;
+    TextView textView_daysResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +36,19 @@ public class DetailActivity extends AppCompatActivity {
 
         imageView_detailPicture = findViewById(R.id.imageView_detailPicture);
 
+        textView_name = findViewById(R.id.textView_name);
+        textView_location = findViewById(R.id.textView_location);
+        textView_disResult = findViewById(R.id.textView_disResult);
+        textView_cityResult = findViewById(R.id.textView_cityResult);
+        textView_weaResult = findViewById(R.id.textView_WeaResult);
+        textView_daysResult = findViewById(R.id.textView_daysResult);
+
         if (this.getIntent().getExtras() != null) {
-            this.picture = this.getIntent().getExtras().getParcelable("picture");
+            m_picture = this.getIntent().getExtras().getParcelable("picture");
             String path = this.getIntent().getStringExtra("path");
             Log.d(TAG, "onCreate: " + path);
 
-            this.picture.setObjectImage(m_DAO.getImageFromFile(path));
+            m_picture.setObjectImage(m_DAO.getImageFromFile(path));
             setupViews();
         }
 
@@ -41,6 +56,8 @@ public class DetailActivity extends AppCompatActivity {
 
     private void setupViews()
     {
-        imageView_detailPicture.setImageBitmap(picture.getObjectImage());
+        imageView_detailPicture.setImageBitmap(m_picture.getObjectImage());
+        textView_name.setText(m_picture.getName());
+        
     }
 }
