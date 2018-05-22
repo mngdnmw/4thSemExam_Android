57e7c5496aaddd1a3c98b3dbb0f2510c19dfcc47
Jeppe Ehmsen <jeppeehmsen@gmail.com>
2018-05-22 13:39:52 +0200

    DAO Comments

--- app/src/main/java/mafioso/so/so/android/DAL/DAO.java
+++ app/src/main/java/mafioso/so/so/android/DAL/DAO.java
@@ -10,7 +10,6 @@ import android.util.Log;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -22,15 +21,19 @@ import mafioso.so.so.android.BE.PictureBE;
 
 public class DAO {
 
+    /** --- Reference to the application context. --- */
     Context context;
 
+    /** --- Firestore DB reference. --- */
     public FirebaseFirestore m_db;
 
+    /** --- Firebase Storage reference --- */
     private FirebaseStorage m_storage;
     StorageReference m_storageRef;
-    String TAG = "SOSOMAFIOSO::DAO";
-
     public static final String FIRE_COLLECTION_PICTURES = "pictures";
+
+    /** --- Tag for debug purposes. --- */
+    String TAG = "SOSOMAFIOSO::DAO";
 
     public DAO(Context context) {
         this.context = context;
@@ -41,31 +44,25 @@ public class DAO {
         this.m_storageRef = m_storage.getReferenceFromUrl("gs://sosomafioso-5c8ef.appspot.com/images");
     }
 
-
-    public void insert(PictureBE picture) {
-        m_db.collection(FIRE_COLLECTION_PICTURES)
-                .add(picture)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
-    }
-
+    /**
+     * Produces a download task for an image stored on firebase.
+     * @param id
+     * Filename for the image to download from firebase.
+     * @return
+     * A task object to download a byte array.
+     */
     private Task<byte[]> getImage(String id) {
         final long MAX_SIZE = 1024 * 1024;
-        //StorageReference ref = m_storage.getReferenceFromUrl("gs://sosomafioso-5c8ef.appspot.com/images/" + id + ".JPG");
         Log.d(TAG, "getImage: Attempting download with path " + m_storageRef.child(id + ".JPG").getPath());
         return m_storageRef.child(id + ".JPG").getBytes(MAX_SIZE);
     }
 
+    /**
+     * Applies an image download to a PictureBE, sets it when downloaded and sends a broadcoast
+     * to update the valid views once completed.
+     * @param picture
+     * The PictureBE to apply an image to.
+     */
     public void applyImage(final PictureBE picture)
     {
         // REPLACE WITH ACTUAL PICTURE ID
@@ -86,12 +83,22 @@ public class DAO {
         });
     }
 
+    /**
+     * Sends an update broadcast to notify that a picture has finished downloading.
+     */
     private void sendMessage() {
         Log.d("sender", "Broadcasting message");
         Intent intent = new Intent("ImgDlComplete");
         LocalBroadcastManager.getInstance(this.context).sendBroadcast(intent);
     }
 
+    /**
+     * Saves a bitmap image as a temporary file in local storage.
+     * @param image
+     * The image to be stored.
+     * @return
+     * Returns the path to the newly created file if successfull, otherwise returns null.
+     */
     public String saveImgToFile(Bitmap image)
     {
         String path = "";
@@ -124,12 +131,24 @@ public class DAO {
         }
     }
 
+    /**
+     * Retrieves bitmap image from a given path.
+     * @param path
+     * Path to a locally stored picture.
+     * @return
+     * Returns found picture as a bitmap, or null if no valid file found.
+     */
     public Bitmap getImageFromFile(String path)
     {
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        Bitmap bitmap = BitmapFactory.decodeFile(path,bmOptions);
+        File file = new File(path);
+        if (file.exists()) {
 
-        return bitmap;
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            Bitmap bitmap = BitmapFactory.decodeFile(path, bmOptions);
+
+            return bitmap;
+        }
+        else { return null; }
     }
 
 
